7cf43921689c57670ba47a0740ad399b45996442
Filip Voska <filip.voska@gmail.com>
2015-11-13 13:06:39 +0100

    logging to file

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ module.exports = {
     'database': 'mongodb://localhost:27017/rznu',
     'ssl_key': fs.readFileSync('./ssl/server.key'),
     'ssl_cert': fs.readFileSync('./ssl/server.crt'),
-    'ssl_ca': fs.readFileSync('./ssl/ca.crt')
+    'ssl_ca': fs.readFileSync('./ssl/ca.crt'),
+    'logFile': './log.txt'
 };
--- main.js
+++ main.js
@@ -1,19 +1,38 @@
 // Express app.
 var config = require('./config.js');
 var express = require('express');
+var router = express.Router();
 var app = express();
 var bodyParser = require('body-parser');
 var uaParser = require('ua-parser-js');
+var fs = require('fs');
 
 // Set parser.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended' : false}));
+app.use(bodyParser.urlencoded({ 'extended' : false }));
 
-var router = express.Router();
 // Logging.
+fs.open(config.logFile, 'wx', function (err, fd) {
+    if (err) {
+        //console.log('Log file ' + require('path').resolve(config.logFile) + ' exists.');
+    }
+    else {
+        fs.close(fd, function (err) {
+            if (!err) {
+                console.log('Log file ' + require('path').resolve(config.logFile) + ' created.');
+            }
+        });
+    }
+});
+console.log('Logging to file: ' + require('path').resolve(config.logFile));
 router.use(function(req, res, next) {
     var ua = uaParser(req.headers['user-agent']);
-    console.log('%s %s %s', req.method, req.path, ua.browser.name + '-v-' + ua.browser.version);
+    var logPart = req.method + '\t' + req.path + '\t' + ua.browser.name + ' ' + ua.browser.version;
+    fs.appendFile(config.logFile, logPart, function (err) {
+        if (err) {
+            console.log('Error saving to log.');
+        }
+    });
     next();
 });
 
@@ -23,11 +42,6 @@ app.use('/api', router);
 
 // Set index page.
 app.use('/', express.static('frontend'));
-/*
-var path = require('path');
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});*/
 
 // Listen on HTTP and HTTPS
 var https = require('https');
